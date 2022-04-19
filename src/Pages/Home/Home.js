ece50e62a845a19170bade556ae3c9895db2e785
shakib-mia <smdshakibmia2001@gmail.com>
2022-04-19 19:48:54 +0600

    extra section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import MoreAbout from './MoreAbour/MoreAbout';
 import Services from './Services/Services';
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
             <div>
                   <Banner></Banner>
                   <Services></Services>
+                  <MoreAbout></MoreAbout>
             </div>
       );
 };
---
+++ src/Pages/Home/MoreAbour/MoreAbout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MoreAbout = () => {
+      return (
+            <div className='container col-12 col-lg-6 mx-auto text-center'>
+                  <h1>Know More about me</h1>
+                  <p>If you want to k now more about me.</p>
+                  <p>You can join with me through <a href="https://www.facebook.com/abdullahalsamad.sakib" target="_blank">Facebook</a></p>
+            </div>
+      );
+};
+
+export default MoreAbout;
